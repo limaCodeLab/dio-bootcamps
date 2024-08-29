8a73d21308d8911891a7417db946bd61e2a4ce89
limaCodeLab <alan.byte.prog@outlook.com>
2024-08-29 10:57:09 -0300

    project created

---
+++ claro-bootcamp/DesafioControleFluxo/src/main/java/com/learning/java/Contador.java
@@ -0,0 +1,33 @@
+package com.learning.java;
+
+
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = input.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = input.nextInt();
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void contar(int parametroUm, int parametroDois) {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        for (int i = parametroUm; i < parametroDois; i++){
+            System.out.println("Imprimindo o numero " + i);
+        }
+    }
+
+
+}
